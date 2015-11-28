8da85aad2715d7e7a47ef14e59d0d2ea75b7ba74
Kyrweeden <lina.vigneron@cri-paris.org>
2015-11-28 16:50:20 +0100

    Print the first parameter

--- exercises/014/solution.py
+++ exercises/014/solution.py
@@ -1,3 +1,5 @@
 import sys
-if (len(sys.argv)) >= 2 : print(sys.argv[1])
-else : print('usage: python3 solution.py PARAM')
+if (len(sys.argv)) >= 2 :
+    print(sys.argv[1])
+else :
+    print('usage: python3 solution.py PARAM')
