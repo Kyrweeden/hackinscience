533e9bf41dd0e859acfd91037b13c0f2d934a534
Kyrweeden <lina.vigneron@cri-paris.org>
2015-12-19 18:54:01 +0100

    Print every two letters pairs

---
+++ exercises/060/solution.py
@@ -0,0 +1,6 @@
+import operator
+first = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+second = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in first:
+    for j in second:
+        print(i+j)
